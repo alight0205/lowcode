07b02302216cb3e912c8adb4c2d82604d1233252
Dujinchao <1778911090@qq.com>
2022-07-29 23:43:37 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from 'antd'
+import './App.less'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">123</div>
+      <Button type="primary">按钮</Button>
+    </div>
+  )
+}
+
+export default App
