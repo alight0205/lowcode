86ff429433ff3fbb83afe9fb13512b48c070298b
song-guoli <2693695870@qq.com>
2022-08-13 12:38:42 +0800

    字体颜色和背景颜色

--- src/pages/home/component/Editor/index.tsx
+++ src/pages/home/component/Editor/index.tsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { CompItem_Workbanch, } from '../../../../utils/interface'
 import { SettingOutlined } from '@ant-design/icons';
-import { Button, InputNumber, Space } from 'antd';
+import {
+  Button, InputNumber, Space,} from 'antd';
 import { changeElement } from '../../../../store/slices/workbanchList'
+import { CompactPicker } from 'react-color';
 import './index.less'
 type State = {
   workbanchList: {
@@ -12,18 +14,18 @@ type State = {
   },
 }
 const ComponentEdit: React.FC = () => {
+  const [width, setWidth] = useState(0)
+  const [color, setColor] = useState('black')
+  const [backgroundcolor, setbackgroundColor] = useState('#fff')
+  const [flag,setFlag]=useState(true)
   const dispatch = useDispatch()
   const EditInfo: any = useSelector<State>(state => state.workbanchList.activeItem);
   const changValue = (e:any) => {
-    // console.log(e.target.value)
     setWidth(e.target.value)
-    // console.log(width)
     e.target.value=''
   }
   const onSubmit = () => {
     console.log(width)
-    // setId(componentId)
-    // console.log(id)
     dispatch( changeElement(
       {
         id:EditInfo.id,
@@ -35,48 +37,66 @@ const ComponentEdit: React.FC = () => {
             style:
             {
               ...EditInfo.args.style,
-               width:width+'px'
+              width: width + 'px',
+              backgroundColor: backgroundcolor,
+              color:color,
               
             }
           }
         }
       }))
+    setbackgroundColor('#fff')
+    setColor('black')
   }
-  const [width, setWidth] = useState(0)
-  // const [id, setId] = useState(0)
+  const onChangeColor = (colorObj:any) => {
+    setColor(colorObj.hex)
+  }
+  // 控制颜色选择器的显示和隐藏
+  const controlColor = () => {
+   setFlag(!flag)
+  }
+  
+  const onChangeCompeleteColor= (colorObj: any) => {
+   
+    console.log(colorObj)
+  }
+  const onChangebackgroundColor: any = (colorObj: any) => {
+    setbackgroundColor(colorObj.hex)
+  }
+  const onChangeCompeletebackgroundColor: any = (colorObj: any)=>{
+    console.log(colorObj)
+  }
+ 
   if (EditInfo) {
     console.log(EditInfo.args.style.width)
     var aaa: any = parseFloat(EditInfo.args.style.width)
-    console.log(EditInfo.id)
-    // var componentId=EditInfo.id
+    var defaultColor:any=EditInfo.args.style.color
+    console.log(EditInfo.args.style)
+    console.log(defaultColor)
   }
   else {
     console.log(EditInfo)
   }
   return <div className="component-edit-container">
-    {/* {JSON.stringify(EditInfo)} */}
     <h2>自定义样式</h2>
     <div className="style">
-   
-     
-      <Space direction="vertical">
-      <span>width:  <InputNumber addonAfter={<SettingOutlined />} placeholder={EditInfo ? aaa.toString() : undefined} onBlur={(e) => changValue(e)} />px
-        <Button onClick={() => onSubmit()}>提交</Button>
-        </span>  
+      <Space direction="vertical"> 
         <span>width:  <InputNumber addonAfter={<SettingOutlined />} placeholder={EditInfo ? aaa.toString() : undefined} onBlur={(e) => changValue(e)} />px
-        <Button onClick={() => onSubmit()}>提交</Button>
-        </span>  
-        <span>width:  <InputNumber addonAfter={<SettingOutlined />} placeholder={EditInfo ? aaa.toString() : undefined} onBlur={(e) => changValue(e)} />px
-        <Button onClick={() => onSubmit()}>提交</Button>
-        </span> 
-        <span>width:  <InputNumber addonAfter={<SettingOutlined />} placeholder={EditInfo ? aaa.toString() : undefined} onBlur={(e) => changValue(e)} />px
-        <Button onClick={() => onSubmit()}>提交</Button>
-        </span>
-        <span>width:  <InputNumber addonAfter={<SettingOutlined />} placeholder={EditInfo ? aaa.toString() : undefined} onBlur={(e) => changValue(e)} />px
-        <Button onClick={() => onSubmit()}>提交</Button>
-      </span>  
-    </Space>
-      
+        </span>   
+      </Space>
+      <span>
+        backgroundcolor: 
+        <div className="bgc" style={{ backgroundColor: backgroundcolor }} onClick={() => controlColor}>修改背景颜色</div>
+        <div className="colorControl" >   <CompactPicker color={backgroundcolor} onChange={onChangebackgroundColor} onChangeComplete={onChangeCompeletebackgroundColor} /> </div>
+      </span>
+      <span>
+        color: 
+        <div className="c" style={{
+          color:color
+        }} onClick={() => controlColor}>修改字体颜色</div>
+        <div className="colorControl" >   <CompactPicker color={color} onChange={onChangeColor} onChangeComplete={onChangeCompeleteColor} /> </div>
+       </span>
+       <Button onClick={() => onSubmit()}>提交</Button>
   </div>
     
   </div>;
